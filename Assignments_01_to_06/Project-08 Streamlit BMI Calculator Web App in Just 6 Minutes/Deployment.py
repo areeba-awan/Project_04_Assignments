90d8e8f8ee84b28f3d4b824fdd3d608e5dc5df1c
Areeba awan <156243202+areeba-awan@users.noreply.github.com>
2025-03-29 20:46:56 +0500

    Delete Assignments_01_to_06 directory

--- Assignments_01_to_06/Project-08 Streamlit BMI Calculator Web App in Just 6 Minutes/Deployment.py
+++
@@ -1,6 +0,0 @@
-Deployment.py
-
-Project : 08 Streamlit BMI Calculator
-
-Deployment Link :
-https://areeba-awan-bmi-calculator.streamlit.app/
--- Assignments_01_to_06/Project-09 Build a Python Website in 15 Minutes With Streamlit/Deployment.py
+++
@@ -1,5 +0,0 @@
-Deployment.py
-
-# Streamlit Deployment Link of Project 09 :
-
-https://areeba-awan-project-09-my-website-with-python.streamlit.app/
